6a265c617358b7e2f90f6daf19b0809baeea23ec
ZP <zairaperez01@gmail.com>
2022-04-23 20:47:37 -0500

    refactored components to use the global state

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+    </div>
+  );
+};
+export default Home;
