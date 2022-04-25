d02857677168d379861a32e95f973915bb6a9d4b
ZP <zairaperez01@gmail.com>
2022-04-24 22:40:16 -0500

    added component and added to cart and remove from cart

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <ProductList />
+      <Cart />
     </div>
   );
 };
+
 export default Home;
